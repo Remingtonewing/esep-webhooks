ca284db49f78af039ecfcc1d7e1df726a1e9205b
Remingtonewing <92884659+Remingtonewing@users.noreply.github.com>
2024-04-03 13:03:53 -0400

    Update FunctionTest.cs

--- test/EsepWebhook.Tests/FunctionTest.cs
+++ test/EsepWebhook.Tests/FunctionTest.cs
@@ -1,20 +1,36 @@
-using Xunit;
+using System.Text;
 using Amazon.Lambda.Core;
-using Amazon.Lambda.TestUtilities;
+using Newtonsoft.Json;
 
-namespace EsepWebhook.Tests;
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-public class FunctionTest
+namespace EsepWebhook;
+
+public class Function
 {
-    [Fact]
-    public void TestToUpperFunction()
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public string FunctionHandler(object input, ILambdaContext context)
     {
+        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+        
+        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
+        
+        var client = new HttpClient();
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+        {
+            Content = new StringContent(payload, Encoding.UTF8, "application/json")
+        };
 
-        // Invoke the lambda function and confirm the string was upper cased.
-        var function = new Function();
-        var context = new TestLambdaContext();
-        var upperCase = function.FunctionHandler("hello world", context);
-
-        Assert.Equal("HELLO WORLD", upperCase);
+        var response = client.Send(webRequest);
+        using var reader = new StreamReader(response.Content.ReadAsStream());
+            
+        return reader.ReadToEnd();
     }
 }
