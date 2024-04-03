d4acd99e31aac7dbf0e090dd05eaa4191482190c
remingtonewing <Rewing@ufl.edu>
2024-04-03 12:54:01 -0400

    Trying to add files

--- obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
+++ obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b568d3994172a1d772d08560386ee0c06b7488")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0263d88222ea022b5a3f0459d1a5e74d55aff662")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
